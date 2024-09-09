6480fff183e8736c6c9768ca4031764bf2271c94
Bryan B <135298241+bryanb-web3@users.noreply.github.com>
2024-09-09 06:49:08 -0400

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-
 import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
 contract Token {
-    string public name = "Aura Lite";
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+    }
 }
