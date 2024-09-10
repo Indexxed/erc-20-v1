345e457754501a30d79f78a67384dd138ed2efe8
Bryan B <135298241+bryanb-web3@users.noreply.github.com>
2024-09-10 02:56:16 -0400

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,14 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-  let token
+  let token, accounts, deployer
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Aura Lite', 'ALIT', '10000000')
+
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', () => {
@@ -21,19 +24,24 @@ describe('Token', () => {
 
     it('has correct name', async () => {
         expect(await token.name()).to.equal(name)
-      })
+    })
     
-      it('has correct symbol', async () => {
+    it('has correct symbol', async () => {
         expect(await token.symbol()).to.equal(symbol)
-      })
+    })
       
-      it('has correct decimals', async () => {
+    it('has correct decimals', async () => {
         expect(await token.decimals()).to.equal(decimals)
-      })
+    })
     
-      it('has correct total supply', async () => {
+    it('has correct total supply', async () => {
         expect(await token.totalSupply()).to.equal(totalSupply)
-      })
+    })
+
+    it('assigns total supply to deployer', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+    })
+
   })
 
 })
