f0b3e0c57ed0431db82cb1575c9b5098270eb350
Bryan B <135298241+bryanb-web3@users.noreply.github.com>
2024-09-12 19:11:17 -0400

    Preparing for Testnet

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -3,7 +3,7 @@ async function main() {
   const Token = await ethers.getContractFactory("Token")
   
   //Deploy contract
-  const token = await Token.deploy()
+  const token = await Token.deploy('Aura Lite', 'xAURA', '10000000')
   await token.deployed()
   console.log(`Token deployed to: ${token.address}`)
 }
